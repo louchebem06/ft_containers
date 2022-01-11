3a60e3dd4b85be15508f8e68fa28636f761ad817
Ledda Bryan <louchebem06@gmail.com>
2022-01-11 15:56:15 +0100

    Stack OK

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 /*   By: bledda <bledda@student.42nice.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/04 17:05:55 by bledda            #+#    #+#             */
-/*   Updated: 2022/01/08 07:11:11 by bledda           ###   ########.fr       */
+/*   Updated: 2022/01/11 15:42:06 by bledda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,90 +18,94 @@
 #include "utils/utility.hpp"
 #include "utils/algorithm.hpp"
 #include <list>
+#include "stack.hpp"
+#include <stack>
 
 #ifndef TESTED_NAMESPACE
 # define TESTED_NAMESPACE ft
 #endif
 
-#define T_SIZE_TYPE typename TESTED_NAMESPACE::vector<T>::size_type
-
-template <typename T>
-void	printSize(TESTED_NAMESPACE::vector<T> const &vct, bool print_content = true)
+template <typename T_STACK>
+void	printSize(T_STACK &stck, bool print_content = 1)
 {
-	const T_SIZE_TYPE size = vct.size();
-	const T_SIZE_TYPE capacity = vct.capacity();
-	const std::string isCapacityOk = (capacity >= size) ? "OK" : "KO";
-	// Cannot limit capacity's max value because it's implementation dependent
-
-	std::cout << "size: " << size << std::endl;
-	std::cout << "capacity: " << isCapacityOk << std::endl;
-	std::cout << "max_size: " << vct.max_size() << std::endl;
+	std::cout << "size: " << stck.size() << std::endl;
 	if (print_content)
 	{
-		typename TESTED_NAMESPACE::vector<T>::const_iterator it = vct.begin();
-		typename TESTED_NAMESPACE::vector<T>::const_iterator ite = vct.end();
-		std::cout << std::endl << "Content is:" << std::endl;
-		for (; it != ite; ++it)
-			std::cout << "- " << *it << std::endl;
+		std::cout << std::endl << "Content was:" << std::endl;
+		while (stck.size() != 0) {
+			std::cout << "- " << stck.top() << std::endl;
+			stck.pop();
+		}
 	}
 	std::cout << "###############################################" << std::endl;
 }
 
+template <typename T>
 class foo {
 	public:
-		foo(void) { };
-		~foo(void) { };
+		typedef T	value_type;
+
+		foo(void) : value(), _verbose(false) { };
+		foo(value_type src, const bool verbose = false) : value(src), _verbose(verbose) { };
+		foo(foo const &src, const bool verbose = false) : value(src.value), _verbose(verbose) { };
+		~foo(void) { if (this->_verbose) std::cout << "~foo::foo()" << std::endl; };
 		void m(void) { std::cout << "foo::m called [" << this->value << "]" << std::endl; };
 		void m(void) const { std::cout << "foo::m const called [" << this->value << "]" << std::endl; };
-		foo &operator=(int src) { this->value = src; return *this; };
-		int getValue(void) const { return this->value; };
+		foo &operator=(value_type src) { this->value = src; return *this; };
+		foo &operator=(foo const &src) {
+			if (this->_verbose || src._verbose)
+				std::cout << "foo::operator=(foo) CALLED" << std::endl;
+			this->value = src.value;
+			return *this;
+		};
+		value_type	getValue(void) const { return this->value; };
+		void		switchVerbose(void) { this->_verbose = !(this->_verbose); };
+
+		operator value_type(void) const {
+			return value_type(this->value);
+		}
 	private:
-		int	value;
+		value_type	value;
+		bool		_verbose;
 };
 
-std::ostream	&operator<<(std::ostream &o, foo const &bar) {
+template <typename T>
+std::ostream	&operator<<(std::ostream &o, foo<T> const &bar) {
 	o << bar.getValue();
 	return o;
 }
 
-// #define TESTED_TYPE int
-#define TESTED_TYPE std::string
-
-void	checkErase(TESTED_NAMESPACE::vector<TESTED_TYPE> const &vct,
-					TESTED_NAMESPACE::vector<TESTED_TYPE>::const_iterator const &it)
-{
-	static int i = 0;
-	std::cout << "[" << i++ << "] " << "erase: " << it - vct.begin() << std::endl;
-	printSize(vct);
-}
+#define TESTED_TYPE foo<int>
+#define t_stack_ TESTED_NAMESPACE::stack<TESTED_TYPE>
+typedef t_stack_::container_type container_type;
 
 int		main(void)
 {
-	TESTED_NAMESPACE::vector<TESTED_TYPE> vct(10);
+	container_type	ctnr;
 
-	for (unsigned long int i = 0; i < vct.size(); ++i)
-		vct[i] = std::string((vct.size() - i), i + 65);
-	printSize(vct);
+	ctnr.push_back(21);
+	ctnr.push_back(42);
+	ctnr.push_back(1337);
+	ctnr.push_back(19);
+	ctnr.push_back(0);
+	ctnr.push_back(183792);
 
-	checkErase(vct, vct.erase(vct.begin() + 2));
+	t_stack_		stck(ctnr);
 
-	checkErase(vct, vct.erase(vct.begin()));
-	checkErase(vct, vct.erase(vct.end() - 1));
+	std::cout << "empty: " << stck.empty() << std::endl;
+	std::cout << "size: " << stck.size() << std::endl;
 
-	checkErase(vct, vct.erase(vct.begin(), vct.begin() + 3));
-	checkErase(vct, vct.erase(vct.end() - 3, vct.end() - 1));
+	stck.push(1);
+	stck.push(2);
+	stck.push(3);
+	stck.push(4);
+	stck.push(5);
+	stck.push(6);
 
-	vct.push_back("Hello");
-	vct.push_back("Hi there");
-	printSize(vct);
-	checkErase(vct, vct.erase(vct.end() - 3, vct.end()));
+	std::cout << "Added some elements" << std::endl;
 
-	vct.push_back("ONE");
-	vct.push_back("TWO");
-	vct.push_back("THREE");
-	vct.push_back("FOUR");
-	printSize(vct);
-	checkErase(vct, vct.erase(vct.begin(), vct.end()));
+	std::cout << "empty: " << stck.empty() << std::endl;
+	printSize(stck);
 
 	return (0);
 }
---
+++ stack.hpp
@@ -0,0 +1,96 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   stack.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bledda <bledda@student.42nice.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/11 15:12:54 by bledda            #+#    #+#             */
+/*   Updated: 2022/01/11 15:55:37 by bledda           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "vector.hpp"
+#include <vector>
+
+namespace ft
+{
+	template <class T, class Container = vector<T> >
+	class stack
+	{
+		public:
+			typedef T				value_type;
+			typedef Container		container_type;
+			typedef unsigned int	size_type;
+		protected:
+			container_type _container;
+		public:
+			explicit stack (const container_type& ctnr = container_type()) {
+				this->_container = ctnr;
+			};
+			
+			bool empty() const { return (this->_container.empty()); };
+			size_type size() const { return (this->_container.size()); };
+			
+			value_type& top() { return (this->_container.back()); };
+			const value_type& top() const { return (this->_container.back()); };
+
+			void push (const value_type& val) { this->_container.push_back(val); };
+
+			void pop () { this->_container.pop_back(); };
+
+			template <class T_, class Container_>
+			friend bool operator==
+			(const stack<T_, Container_>& lhs, const stack<T_, Container_>& rhs);
+
+			template <class T_, class Container_>
+			friend bool operator!=
+			(const stack<T_, Container_>& lhs, const stack<T_, Container_>& rhs);
+
+			template <class T_, class Container_>
+			friend bool operator<
+			(const stack<T_, Container_>& lhs, const stack<T_, Container_>& rhs);
+
+			template <class T_, class Container_>
+			friend bool operator<=
+				(const stack<T_, Container_>& lhs, const stack<T_, Container_>& rhs);
+
+			template <class T_, class Container_>
+			friend bool operator>
+				(const stack<T_, Container_>& lhs, const stack<T_, Container_>& rhs);
+
+			template <class T_, class Container_>
+			friend bool operator>=
+				(const stack<T_, Container_>& lhs, const stack<T_, Container_>& rhs);
+	};
+
+	template <class T_, class Container_>
+	bool operator== (const stack<T_, Container_>& lhs, const stack<T_, Container_>& rhs) {
+		return (lhs._container == rhs._container);
+	};
+
+	template <class T_, class Container_>
+	bool operator!= (const stack<T_, Container_>& lhs, const stack<T_, Container_>& rhs) {
+		return (lhs._container != rhs._container);
+	};
+
+	template <class T_, class Container_>
+	bool operator<  (const stack<T_, Container_>& lhs, const stack<T_, Container_>& rhs) {
+		return (lhs._container < rhs._container);
+	};
+
+	template <class T_, class Container_>
+	bool operator<= (const stack<T_, Container_>& lhs, const stack<T_, Container_>& rhs) {
+		return (lhs._container <= rhs._container);
+	};
+
+	template <class T_, class Container_>
+	bool operator>  (const stack<T_, Container_>& lhs, const stack<T_, Container_>& rhs) {
+		return (lhs._container > rhs._container);
+	};
+
+	template <class T_, class Container_>
+	bool operator>= (const stack<T_, Container_>& lhs, const stack<T_, Container_>& rhs) {
+		return (lhs._container >= rhs._container);
+	};
+}
