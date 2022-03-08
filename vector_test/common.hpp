1c2d28c2b661ae67460fa314533c58154a50c4ff
Ledda Bryan <louchebem06@gmail.com>
2022-03-08 02:01:13 +0100

    update

---
+++ vector_test/common.hpp
@@ -0,0 +1,72 @@
+#pragma once
+
+#include "../vector.hpp"
+#include <vector>
+#include <iostream>
+#include <string>
+
+#ifndef TESTED_NAMESPACE
+# define TESTED_NAMESPACE ft
+#endif
+
+#define T_SIZE_TYPE typename TESTED_NAMESPACE::vector<T>::size_type
+
+template <typename T>
+void	printSize(TESTED_NAMESPACE::vector<T> const &vct, bool print_content = true)
+{
+	const T_SIZE_TYPE size = vct.size();
+	const T_SIZE_TYPE capacity = vct.capacity();
+	const std::string isCapacityOk = (capacity >= size) ? "OK" : "KO";
+	// Cannot limit capacity's max value because it's implementation dependent
+
+	std::cout << "size: " << size << std::endl;
+	std::cout << "capacity: " << isCapacityOk << std::endl;
+	std::cout << "max_size: " << vct.max_size() << std::endl;
+	if (print_content)
+	{
+		typename TESTED_NAMESPACE::vector<T>::const_iterator it = vct.begin();
+		typename TESTED_NAMESPACE::vector<T>::const_iterator ite = vct.end();
+		std::cout << std::endl << "Content is:" << std::endl;
+		for (; it != ite; ++it)
+			std::cout << "- " << *it << std::endl;
+	}
+	std::cout << "###############################################" << std::endl;
+}
+
+class foo {
+	public:
+		foo(void) { };
+		~foo(void) { };
+		void m(void) { std::cout << "foo::m called [" << this->value << "]" << std::endl; };
+		void m(void) const { std::cout << "foo::m const called [" << this->value << "]" << std::endl; };
+		foo &operator=(int src) { this->value = src; return *this; };
+		int getValue(void) const { return this->value; };
+	private:
+		int	value;
+};
+
+std::ostream	&operator<<(std::ostream &o, foo const &bar) {
+	o << bar.getValue();
+	return o;
+}
+
+int		assign(void);
+int		at_const(void);
+int		at(void);
+int		bidirect_it(void);
+int		copy_construct(void);
+int		erase(void);
+int		insert(void);
+int		insert2(void);
+int		ite_arrow(void);
+int		ite_eq_ope(void);
+int		ite(void);
+int		push_pop(void);
+int		relational_ope(void);
+int		rev_ite_construct(void);
+int		rite_arrow(void);
+int		rite_eq_ope(void);
+int		rite(void);
+int		rite2(void);
+int		size(void);
+int		swap(void);
---
+++ vector_test/main.cpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bledda <bledda@student.42nice.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/08 01:40:47 by bledda            #+#    #+#             */
+/*   Updated: 2022/03/08 01:51:46 by bledda           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "common.hpp"
+
+int main(void)
+{
+	assign();
+	at_const();
+	at();
+	bidirect_it();
+	copy_construct();
+	erase();
+	insert();
+	insert2();
+	ite_arrow();
+	ite_eq_ope();
+	ite();
+	push_pop();
+	relational_ope();
+	rev_ite_construct();
+	rite_arrow();
+	rite_eq_ope();
+	rite();
+	rite2();
+	size();
+	swap();
+	return (0);
+}
