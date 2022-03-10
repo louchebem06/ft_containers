0cdb45ae3e14cce1a33e4c9631a1a08dcd2f59b1
Bryan Ledda <bledda@c1r3p2.42nice.fr>
2022-03-10 21:42:38 +0100

    update

--- new_binary/main.cpp
+++ new_binary/main.cpp
@@ -6,20 +6,29 @@
 /*   By: bledda <bledda@student.42nice.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/07 23:40:38 by bledda            #+#    #+#             */
-/*   Updated: 2022/03/10 21:31:07 by bledda           ###   ########.fr       */
+/*   Updated: 2022/03/10 21:40:02 by bledda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "btree.hpp"
 
+#define MAX_RAM 4294967296
+#define BUFFER_SIZE 4096
+struct Buffer
+{
+	int idx;
+	char buff[BUFFER_SIZE];
+};
+
+
+#define COUNT (MAX_RAM / (int)sizeof(Buffer))
+
 int main(void)
 {
 	ft::btree<int, int> test;
 
-	for (int i = 0; i < 100; i++)
-		test.insert(ft::make_pair(random() % 100, 12));
-
-	test.tree();
+	for (int i = 0; i < COUNT; i++)
+		test.insert(ft::make_pair(rand(), rand()));
 
 	ft::btree<int, int>::iterator begin = test.begin();
 	ft::btree<int, int>::iterator end = test.end();
